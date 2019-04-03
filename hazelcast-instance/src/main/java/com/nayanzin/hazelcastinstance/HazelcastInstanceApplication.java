b66862ed373fc12362278363b8f7bdd6baa9011c
Nayanin Alexander <nayanzin.alexander@gmail.com>
2019-04-03 12:36:47 +0300

    Hazelcast demo started

---
+++ hazelcast-instance/src/main/java/com/nayanzin/hazelcastinstance/HazelcastInstanceApplication.java
@@ -0,0 +1,13 @@
+package com.nayanzin.hazelcastinstance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazelcastInstanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HazelcastInstanceApplication.class, args);
+    }
+
+}
---
+++ hazelcast-instance/src/test/java/com/nayanzin/hazelcastinstance/HazelcastInstanceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nayanzin.hazelcastinstance;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HazelcastInstanceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
